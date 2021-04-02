b33737481104df29f092aa593b27cbc5ea12b29d
i-love-fgo <79355329+i-love-fgo@users.noreply.github.com>
2021-04-02 19:00:42 +0900

    占い結果実装1

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -52,13 +52,32 @@ function result (){
       num5 = 1;
     }
     const total_num = num1 + num2 + num3 + num4 + num5 
-    const form = document.getElementById("form");
-    const formData = new FormData(form);
-    const XHR = new XMLHttpRequest();
-    XHR.open("GET", "/dailies", true);
-    XHR.responseType = 'arraybuffer';
-    XHR.send(formData);
+    if (total_num => 15) {
+      // innerHTML
+    } else if (total_num => 10 && total_num <= 6) {
+      // innerHTML
+    } else if (total_num <= 5) {
+      // innerHTML
+    }
+    
+    // XHR.onload = () => {
+    //   const html = `
+    //   <div class="h1><img src="result.png"></div><br>
+    //   <div class="content">
+    //   今日のあなたは、疲れが溜まった『疲労困憊ちゃん』です！<br>頑張るあなたも素敵ですが、しっかり休息をとらないとダウンしちゃうかも・・・<br>
+    //   ご家庭で猫ちゃんを飼われている方は、猫ちゃんが嫌がらない程度に甘えてみてはいかがでしょうか？<br>また、猫ちゃんを飼われていない方は、猫カフェなどに行かれてみるのはいかがでしょうか？<br>
+    //   モフモフにゃんこ達が、頑張るあなたをまったり癒してくれますよ♪
+    //   </div><br>
+    //   <div class="sub-content>
+    //   そんなあなたのラッキーにゃんこは、『スコティッシュフォールド』です！<br>甘えん坊で人懐っこく、近寄るとお腹を見せて転がりアピールしてくることもあるそうです。<br>
+    //   とても寂しがり屋さんなので、甘えてきたらとことん甘やかしてあげましょう！
+    //   </div><br>
+    //   <div class="h3><img src="suko.jpg", size: "640x428"></div>`;
+    // }
+  
   });
 }
 
 window.addEventListener('load', result);
+
+
