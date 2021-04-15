9814bfeaa493c05678fd2398b6fdb3f070fc0a9d
i-love-fgo <79355329+i-love-fgo@users.noreply.github.com>
2021-04-15 17:14:29 +0900

    画像表示完了！

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
@@ -16,5 +15,5 @@ require("../indexxx")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+//const images = require.context('../images', true)
+//const imagePath = (name) => images(name, true)
