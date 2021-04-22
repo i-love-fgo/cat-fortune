96fd6fd02e6380f1f2f736bc9791a5dbdebc71d2
i-love-fgo <79355329+i-love-fgo@users.noreply.github.com>
2021-04-22 20:58:56 +0900

    空のコミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # before_action user_params
 
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     # @user.nickname = current_user.nickname
     # @user.profile_img = current_user.profile_img
     # @user.favorite_cat = current_user.favorite_cat
