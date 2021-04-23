255f84e4b5c8ea65859192df8444910f8c914b2f
i-love-fgo <79355329+i-love-fgo@users.noreply.github.com>
2021-04-23 10:33:26 +0900

    空のコミット

--- app/models/zodiac.rb
+++ app/models/zodiac.rb
@@ -2,7 +2,7 @@ class Zodiac < ApplicationRecord
   belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :sign_id
+  belongs_to_active_hash :sign
   with_options numericality: { other_than: 1} do
     validates :sign_id
   end
