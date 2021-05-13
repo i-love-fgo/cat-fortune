d2b5c1e9808ad6e4702017d27acdfc2fa1939fde
i-love-fgo <79355329+i-love-fgo@users.noreply.github.com>
2021-05-13 10:38:15 +0900

    マイページへのリンク設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+
+  def show
+  end
+  
+  private
   def user_params
     params.require(:user).permit(:nickname, :email, :favorite_cat, :image)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'fortunes#index'
+  resources :users, only: :show
   resources :fortunes, only: :index
   resources :dailies, only: :index
   resources :bloodtypes, only: :index
